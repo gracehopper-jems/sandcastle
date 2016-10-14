98072975c90133d7dacdd0501d99a29364200b6a
Thomas Kelly <tmkelly28@gmail.com>
2016-10-13 21:24:06 -0400

    update

---
+++ browser/SinglePuppy.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class SinglePuppy extends Component {
+
+  render () {
+    return (
+      <div>
+        <h2>PUPPY NAME GOES HERE</h2>
+        <div>
+          <img src="http://placehold.it/400x400" />
+        </div>
+      </div>
+    )
+  }
+}
