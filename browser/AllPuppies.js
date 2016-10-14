a7b39147eb6543eaac7201b1453d3ed7f3b1e6ca
Thomas Kelly <tmkelly28@gmail.com>
2016-10-13 21:22:28 -0400

    updating

--- browser/AllPuppies.js
+++ browser/AllPuppies.js
@@ -4,14 +4,12 @@ export default class AllPuppies extends React.Component {
 
   render () {
     return (
-      <div className="container flexbox-container">
-        <div className="jumbotron">
-          <ul className="list-unstyled">
-            <li><a href="#">PUPPY NAME GOES HERE</a></li>
-            <li><a href="#">PUPPY NAME GOES HERE</a></li>
-            <li><a href="#">PUPPY NAME GOES HERE</a></li>
-          </ul>
-        </div>
+      <div>
+        <ul className="list-unstyled">
+          <li><a href="#">PUPPY NAME GOES HERE</a></li>
+          <li><a href="#">PUPPY NAME GOES HERE</a></li>
+          <li><a href="#">PUPPY NAME GOES HERE</a></li>
+        </ul>
       </div>
     )
   }
--- browser/index.js
+++ browser/index.js
@@ -5,8 +5,10 @@ import ReactDOM from 'react-dom';
 import AllPuppies from './AllPuppies';
 
 ReactDOM.render(
-  <div>
-    <AllPuppies />
+  <div className="container flexbox-container">
+    <div className="jumbotron">
+      <AllPuppies />
+    </div>
   </div>,
   document.getElementById('app')
 );
