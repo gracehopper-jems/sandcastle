718f2cef33f6449ab3660f6fc41cb5c40571463a
Thomas Kelly <tmkelly28@gmail.com>
2017-02-14 18:47:49 -0500

    upgrade to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,20 @@
-
-'use strict';
-
-var webpack = require('webpack');
-
 module.exports = {
   entry: './browser/index.js',
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
-  context: __dirname,
   devtool: 'source-map',
   module: {
-    loaders: [
+    rules: [
       {
-        test: /js$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
           presets: ['react', 'es2015']
         }
       }
     ]
-  },
+  }
 };
