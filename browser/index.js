bbf989fd87af20f66b796b48089bf2f81a8eca3e
Jin Wang <jin.wang.527@gmail.com>
2017-03-16 15:39:55 -0400

    add index.js router

--- browser/index.js
+++ browser/index.js
@@ -3,10 +3,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TextEditor from './TextEditor';
+import {Router, Route, hashHistory, IndexRedirect} from 'react-router';
+
 
 ReactDOM.render(
-  <div>
-  	<TextEditor /> 
-  </div>,
+  <Router history={hashHistory}>
+    <Route path="/" component={AppContainer}>
+      <Route path="/html" component={htmlContainer} />
+      <Route path="/css" component={cssContainer} />
+      <Route path="/javascript" component={javascriptContainer} />
+      <Route path="/express" component={expressContainer} />
+      <Route path="/sequelize" component={sequelizeContainer} />
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
+
