53dea38310b76de02841d44af05c094691084e79
Jin Wang <jwang@Jins-MacBook-Air.local>
2017-03-19 18:21:30 -0400

    add code reducer

---
+++ browser/reducers/code.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const initialState = {
+  htmlString: '',
+  cssString: '',
+  jsString: '',
+  serverString: '',
+  databaseString: ''
+};
+
+// reducer
+const reducer = (state=initialState, action) => {
+  const newState = Object.assign({}, state);
+  switch(action.type) {
+    case RECEIVE_HTML:
+      newState.htmlString = action.htmlString;
+      return newState;
+
+    default:
+      return state;
+  }
+};
+
+// constants
+const RECEIVE_HTML = 'RECEIVE_HTML';
+
+// action creators
+export const receiveHTML = htmlString => ({
+  type: RECEIVE_HTML,
+  htmlString
+});
+
+export const updateHTML = (htmlString) => {
+  return dispatch => {
+    dispatch(receiveHTML(htmlString));
+  };
+};
+
+export default reducer;
