5aedb684e8cd42882343e55ce051d38363edd0e8
Susanna Huang <dearsusannahuang@gmail.com>
2017-03-20 20:40:05 -0400

    added js save button

--- browser/Containers/JSEditor.js
+++ browser/Containers/JSEditor.js
@@ -6,6 +6,10 @@ export default class JSEditor extends React.Component {
   constructor(props){
     super(props)
     this.init = this.init.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+    this.state = {
+      text: null
+    }
   }
 
 
@@ -37,23 +41,32 @@ export default class JSEditor extends React.Component {
     var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror, {
       richTextShortcuts: false,
       richTextToolbar: false,
-      defaultText: 'HTML here!'
+      defaultText: 'JS here!'
     });
 
     const self = this;
     firepad.on('ready', function() {
       // Firepad is ready.
-      self.props.handlers.handleJSUpdate(firepad.getText());
+      self.setState({
+        text: firepad.getText()
+      })
     });
     firepad.on('synced', function(isSynced) {
       // isSynced will be false immediately after the user edits the pad,
       // and true when their edit has been saved to Firebase.
       if (isSynced) {
-        self.props.handlers.handleJSUpdate(firepad.getText());
+         self.setState({
+         text: cssFirepad.getText()
+        })
       }
     });
+ }
 
+  handleSave(){
+    const self = this;
+    self.props.handlers.handleJSUpdate(this.state.text);
   }
+
 
   render() {
     console.log('props in js', this.props);
@@ -62,6 +75,10 @@ export default class JSEditor extends React.Component {
         <div className="row">
           <div className="col-md-6">
             <div id="javascriptFirepad"></div>
+            <button
+              type="button"
+              className="btn btn-info"
+              onClick={this.handleSave}>SAVE JS</button>
           </div>
           <div className="col-md-6">
             <img src="https://storage.googleapis.com/material-design/publish/material_v_10/assets/0Bx4BSt6jniD7MG80dmpHT0RidGs/style_icons_system_intro_principles_actionable.png"></img>
