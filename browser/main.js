eff004307ce366654333c08c8cd3e5fc5a20c914
Susanna Huang <dearsusannahuang@gmail.com>
2017-03-24 11:21:40 -0400

    changed setinterval to settimeout and added autorefresh to cm

--- browser/main.js
+++ browser/main.js
@@ -42,10 +42,10 @@ const onAppEnter = () => {
       const codeMirrorInstances = pads[1];
       // we can add codemirror instances to store state if necessary
       // store.dispatch(handleCodeMirrorInstances(codeMirrorInstances));
-      setInterval(() => {
+      setTimeout(() => {
         console.log('refreshing code mirrors');
         pads[1].forEach(pad => {pad.refresh();});
-      }, 500);
+      }, 1);
 
       madeFirepads = true;
       let count = 0;
