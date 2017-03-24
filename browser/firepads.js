d57a4eb52871cc443eea5e9ca2236af47a61277f
Jin Wang <jwang@Jins-MacBook-Air.local>
2017-03-24 09:09:00 -0400

    small edits to formatting

--- browser/firepads.js
+++ browser/firepads.js
@@ -12,7 +12,13 @@ export default function makeFirepads() {
 	var serverFirepadRef = firebase.database().ref(`/users/${userId}/server`);
 	var databaseFirepadRef = firebase.database().ref(`/users/${userId}/database`);
 
-	let firepads = [{ id: 'html-firepad-container', mode: 'xml' }, { id: 'css-firepad-container', mode: 'css'}, { id: 'js-firepad-container', mode: 'javascript'}, {id: 'server-firepad-container', mode: 'javascript'}, {id: 'db-firepad-container', mode: 'javascript'}];
+	let firepads = [
+		{ id: 'html-firepad-container', mode: 'xml' },
+		{ id: 'css-firepad-container', mode: 'css'},
+		{ id: 'js-firepad-container', mode: 'javascript'},
+		{id: 'server-firepad-container', mode: 'javascript'},
+		{id: 'db-firepad-container', mode: 'javascript'}
+	];
 
 	let fires = firepads.map(firepad => {
 		return CodeMirror(document.getElementById(firepad.id), {
