8bbbc3459bf2cf27feb067d0bd9a311e4c7ff1c7
Jin Wang <jin.wang.527@gmail.com>
2017-03-24 09:09:00 -0400

    small edits to formatting

--- browser/main.js
+++ browser/main.js
@@ -40,12 +40,12 @@ const onAppEnter = () => {
       let pads = makeFirepads();
 
       const codeMirrorInstances = pads[1];
+      // we can add codemirror instances to store state if necessary
       // store.dispatch(handleCodeMirrorInstances(codeMirrorInstances));
-
       setInterval(() => {
         console.log('refreshing code mirrors');
         pads[1].forEach(pad => {pad.refresh();});
-      }, 1000);
+      }, 500);
 
       madeFirepads = true;
       let count = 0;
