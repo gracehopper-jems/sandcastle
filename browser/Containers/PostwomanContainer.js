1407360091bf9f8e8c7092de725db3aad0865a8f
Jin Wang <jin.wang.527@gmail.com>
2017-03-24 15:09:33 -0400

    add onchange and onclick to form

--- browser/Containers/PostwomanContainer.js
+++ browser/Containers/PostwomanContainer.js
@@ -1,12 +1,38 @@
 import React, {Component} from 'react';
 import { FormGroup, InputGroup, FormControl, DropdownButton, MenuItem, Button } from 'react-bootstrap'
+import axios from 'axios';
 
 export default class PostwomanContainer extends Component {
     constructor(props){
         super(props)
+        this.state = {
+            path: '/'
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSend = this.handleSend.bind(this);
+    }
+
+    handleChange(event) {
+        event.preventDefault();
+        let path = event.target.value;
+        if (path[0] !== '/') {
+            path = '/' + path;
+        }
+        this.setState({path: path});
+    }
+
+    handleSend(event) {
+        event.preventDefault();
+
+        axios.post('/postWomanGetPath', {path: this.state.path})
+        .then(() => {
+
+        })
+        .catch(console.error);
     }
 
     render(){
+        console.log('state', this.state)
         return (
             <div>
                 <FormGroup>
@@ -19,9 +45,9 @@ export default class PostwomanContainer extends Component {
                         <MenuItem key="1">POST</MenuItem>
                         <MenuItem key="2">PUT</MenuItem>
                         </DropdownButton>
-                        <FormControl type="text" />
+                        <FormControl type="text" value={this.state.path} onChange={this.handleChange} />
                         <InputGroup.Button>
-                        <Button>Send</Button>
+                        <Button onClick={this.handleSend}>Send</Button>
                         </InputGroup.Button>
                     </InputGroup>
                     </FormGroup>
--- server.js
+++ server.js
@@ -37,20 +37,20 @@ module.exports = app
 
   // adding userid to req.session
   .post('/setUser', (req, res, next) => {
-      console.log("SETTING USER"); 
-      console.log("REQ BODY USER ID", req.body.userId); 
+      console.log("SETTING USER");
+      console.log("REQ BODY USER ID", req.body.userId);
       const userId = req.body.userId;
       req.session.userId = userId;
       res.sendStatus(200);
   })
 
   .get('/removeUser', (req, res, next) => {
-    req.session.destroy(); 
+    req.session.destroy();
     res.sendStatus(200);
   })
 
   .post('/container', (req, res, next) => {
-    const userId = req.session.userId.toLowerCase(); 
+    const userId = req.session.userId.toLowerCase();
     const userRoutes = req.body.userRoutes;
     const userModels = req.body.userModels;
     runContainer(userId, 3001, 6542, userRoutes, userModels);
@@ -59,7 +59,7 @@ module.exports = app
   })
 
   // run a get request in container terminal and receive the result
-  .get('/containerTest', (req, res, next) => {
+  .get('/postWomanGetPath', (req, res, next) => {
     // the container's name is the user id plus `app_docker-test_1`
     const userId = req.session.userId.toLowerCase();
     const containerName = `${userId}app_docker-test_1`;
@@ -75,7 +75,7 @@ module.exports = app
   })
 
     .get('/containerPostTest', (req, res, next) => {
-        const userId = req.session.userId.toLowerCase(); 
+        const userId = req.session.userId.toLowerCase();
         const containerName = `${userId}app_docker-test_1`;
         exec(`docker ps -aqf "name=${containerName}"`)
         .then( (containerId) => {
