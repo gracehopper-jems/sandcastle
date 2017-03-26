301d3a5d6416d52e377f562f34712b02e5fe479c
Jin Wang <jin.wang.527@gmail.com>
2017-03-26 11:18:56 -0400

    user app works with ajax request

--- docker/userServer.js
+++ docker/userServer.js
@@ -8,6 +8,7 @@ const userRoutes = require('./userRoutes');
 const operation = retry.operation({ retries: 3 });
 const bodyParser = require('body-parser');
 const {resolve} = require('path');
+const axios = require('axios');
 
 // Constants
 const PORT = 8080;
