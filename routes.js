9e271f353d397abdad3f088d47c15f9078bee323
Jin Wang <jin.wang.527@gmail.com>
2017-03-27 17:33:28 -0400

    update argsObj

--- routes.js
+++ routes.js
@@ -21,16 +21,7 @@ module.exports = router
 
   .post('/container', (req, res, next) => {
     if (req.session.userId){
-
-      const argsObj = {
-        // userId: req.session.userId.toLowerCase(),
-        userRoutes: req.body.userRoutes,
-        userModels: req.body.userModels,
-        userHTML: req.body.userHTML,
-        userCSS: req.body.userCSS,
-        userJS: req.body.userJS,
-      }
-
+      const argsObj = req.body;
       argsObj.userId = req.session.userId.toLowerCase();
 
       // find a port that is available
