a8e115be6a93e096277d5f9a222ac04cfd787163
Susanna Huang <dearsusannahuang@gmail.com>
2017-03-27 17:01:03 -0400

    Containers have indenting fixed

--- browser/Containers/DatabaseTable.js
+++ browser/Containers/DatabaseTable.js
@@ -2,63 +2,43 @@ import React, {Component} from 'react';
 
 
 export default class DatabaseTable extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-    	entries: [], 
-    	headings: [], 
-    }
+    constructor(props){
+        super(props)
+        this.state = {
+            entries: [],
+            headings: [],
+        }
   }
+    componentWillReceiveProps(nextProps){
+        const database = nextProps.database;
+        if (database.length){
+            const entries = database.map(item => JSON.parse(item));
+            this.setState({entries: entries});
 
+            const headings = Object.keys(entries[0]);
+            this.setState({headings: headings});
+          }
+      }
+    render() {
+        let count = 0;
+        return (
+            <table>
+                <tbody>
+                    <tr>
+                        {this.state.headings.map(heading => <th key={heading}>{heading}</th>) }
+                    </tr>
+                 </tbody>
 
-
-	componentWillReceiveProps(nextProps){ 
-  		const database = nextProps.database; 
-
-  		if (database.length){
-  		  	
-  		  	const entries = database.map(item => JSON.parse(item)); 
-		  	this.setState({entries: entries}); 
-
-		  	const headings = Object.keys(entries[0]); 
-		  	this.setState({headings: headings}); 
-
-  		}
-
-  	}
-
-
-	render() {
-		let count = 0; 
-		return (
-
-			<table> 
-				<tbody>
-					<tr> 
-						{this.state.headings.map(heading => <th key={heading}>{heading}</th>) }
-					</tr> 
-				 </tbody>
-
-			{this.state.entries.map(entry => { 
-				return (
-					 <tbody key={"" + count++}>
-					 	<tr key={"" + count++}>
-					 		{this.state.headings.map( col => <td key={"" + count++}>{entry[col]}</td>) } 
-					 	</tr> 
-					 </tbody> 
-				) 
-			})} 
-
-			</table>
-		) 
-	} 
-} 
-
-
-
-
-		  
-
-
-
-
+            {this.state.entries.map(entry => {
+                return (
+                     <tbody key={"" + count++}>
+                         <tr key={"" + count++}>
+                             {this.state.headings.map( col => <td key={"" + count++}>{entry[col]}</td>) }
+                         </tr>
+                     </tbody>
+                )
+            })}
+            </table>
+        )
+    }
+}
--- browser/Containers/Frontend.js
+++ browser/Containers/Frontend.js
@@ -2,23 +2,23 @@ import React, {Component} from 'react';
 import { Link } from 'react-router';
 
 export default class Frontend extends Component {
-  constructor(props){
+    constructor(props){
     super(props)
-  }
+    }
 
     render(){
-      console.log("Inside FrontEnd rendering the entire page", this.props.children);
-        return(
+        console.log("Inside FrontEnd rendering the entire page", this.props.children);
+        return (
             <div className="container">
                 <nav className="navbar navbar-default" className="inner-nav">
-                  <div className="container-fluid">
-                      <ul className="nav navbar-nav">
+                    <div className="container-fluid">
+                        <ul className="nav navbar-nav">
                         <li><Link to="/html">HTML</Link></li>
-                          <li><Link to="/css">CSS</Link></li>
-                          <li><Link to="/javascript">Javascript</Link></li>
-                      </ul>
-                  </div>
-              </nav>
+                            <li><Link to="/css">CSS</Link></li>
+                            <li><Link to="/javascript">Javascript</Link></li>
+                        </ul>
+                    </div>
+                </nav>
                 <div>
                     {this.props.children}
                 </div>
