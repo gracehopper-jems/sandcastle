86773b63ea54f6baf9843f59c69bd2f51ea8e726
mlkiung <meredith.kiung@gmail.com>
2017-03-27 19:24:06 -0400

    firepads promisified and functioning

--- browser/utils/makeFrontendIframe.js
+++ browser/utils/makeFrontendIframe.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import store from '../store';
+import { Promise } from 'bluebird';
 
 const makeFrontendIframe = () => {
 	let state = store.getState();
-	console.log('STATE IN makeFrontendIframe', state);
 	// Create a new blank iframe
 	var newIframe = document.createElement('iframe');
 	// Set attributes for iFrame (do whatever suits)
@@ -18,14 +18,14 @@ const makeFrontendIframe = () => {
 
 	// List any CSS you want to reference within the iframe
 	// var CSS = '<link rel="stylesheet" href="https://external.com/css/styles.css">';
-	let CSS = '<style>' + state.code.cssString + '</style>';
+	let CSS = `<style>${state.code.cssString}</style>`;
 
 	// List any JS you want to reference within the iframe
 	// var JS = '<script src="http://external.com/js/plugins.js"></script>';
-	let JS = '<script>' + state.code.jsString + '</script>';
+	let JS = `<script>${state.code.jsString}</script>`;
 
 	// Now sticch it all together into one thing to insert into the iframe
-	var myContent = '<!DOCTYPE html>' + '<html><head><title>Rendered HTML from Pattern</title>' + CSS + '</head><body>' + getHTML + JS + '</body></html>';
+	var myContent = `<!DOCTYPE html><html><head><title>Rendered HTML from Pattern</title>${CSS}</head><body>${getHTML}${JS}</body></html>`;
 
 	// Use the JavaScript methods to write to the iFrame, then close it
 	newIframe.contentWindow.document.open('text/html', 'replace');
