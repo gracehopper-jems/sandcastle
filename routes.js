d2f3c14b3365876b0296ce7f090185377c4f4255
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-03-28 14:45:13 -0400

    completed

--- routes.js
+++ routes.js
@@ -33,56 +33,30 @@ module.exports = router
         let serverPort;
         let postgresPort;
 
-<<<<<<< HEAD
         // find available port
         portfinder.getPortPromise()
-            .then((port) => {
-                serverPort = port;
-                console.log('server port', serverPort);
-                portfinder.basePort = port + 1;
-                return portfinder.getPortPromise()
-            })
-            .then((port) => {
-                postgresPort = port;
-                console.log('server port', postgresPort);
-                return runContainer(Object.assign({}, argsObj, {
-                    serverPort,
-                    postgresPort
-                }));
-            })
-            .then(() => {
-                // send response with port number
-                res.send({
-                    response: 'running container on port',
-                    port: serverPort
-                });
-            })
-            .catch(console.error);
-=======
-      // find available port
-      portfinder.getPortPromise()
-      .then((port) => {
-        serverPort = port;
-        console.log('server port', serverPort);
-        portfinder.basePort = port + 1;
-        return portfinder.getPortPromise()
-      })
-      .then((port) => {
-        postgresPort = port;
-        console.log('postgres port', postgresPort);
-        return runContainer(Object.assign({}, argsObj, {serverPort, postgresPort}));
-      })
-      .then(() => {
-        // send response with port number
-        res.send({response: 'running container on port', port: serverPort});
-      })
-      .catch(console.error);
->>>>>>> master
+        .then((port) => {
+            serverPort = port;
+            console.log('server port', serverPort);
+            portfinder.basePort = port + 1;
+            return portfinder.getPortPromise()
+        })
+        .then((port) => {
+            postgresPort = port;
+            console.log('postgres port', postgresPort);
+            return runContainer(Object.assign({}, argsObj, {serverPort, postgresPort}));
+        })
+        .then(() => {
+            // send response with port number
+            res.send({response: 'running container on port', port: serverPort});
+        })
+        .catch(console.error);
 
     } else {
         res.send('no logged in user');
     }
 })
+
 
 .get('/removeContainer', (req, res, next) => {
     if (req.session.userId) {
@@ -145,19 +119,9 @@ module.exports = router
         .catch(console.error);
 })
 
-<<<<<<< HEAD
 .delete('/containerAPI', (req, res, next) => {
     exec(`docker ps -aqf "name=${containerName}"`)
         .then((containerId) => {
-=======
-    .delete('/containerDelete', (req, res, next) => {
-      if (req.session.userId){
-        const path = req.session.path;
-        const userId = req.session.userId.toLowerCase();
-        const containerName = `${userId}app_docker-test_1`;
-        exec(`docker ps -aqf "name=${containerName}"`)
-        .then( (containerId) => {
->>>>>>> master
             return exec(`docker exec ${containerId.trim()} curl -X "DELETE" http://localhost:8080${path.trim()}`)
         })
         .then((result) => {
