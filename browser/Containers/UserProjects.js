a57685f54fb8f09dada68cb4eabc5b019505f48a
Susanna Huang <dearsusannahuang@gmail.com>
2017-03-28 23:33:47 -0400

    conflicts fixed

--- browser/Containers/UserProjects.js
+++ browser/Containers/UserProjects.js
@@ -19,11 +19,11 @@ export default class UserProjects extends Component {
   }
 
   handleToggle(){
-      return axios.get(`/api/${this.props.user.userId}`)
+      return axios.get(`/api/user/${this.props.user.userId}`)
       .then((projects) => {
           console.log("getting the projects", this.state.projects);
           this.setState({open: !this.state.open, projects: projects.data});
-          console.log("getting the projects affter", this.state.projects);
+          console.log("getting the projects after", this.state.projects);
       })
       .catch(console.error)
   }
@@ -35,14 +35,13 @@ export default class UserProjects extends Component {
   handleOpenProject(hashedProjectId){
     return axios.get(`api/project/${hashedProjectId}`)
     .then((project) => {
-        console.log('GETTING YOUR PROJECT', JSON.parse(project.data.code));
+        console.log('GETTING YOUR PROJECT', (project.data.code));
         console.log('DAPROPS', this.props)
         return JSON.parse(project.data.code)
     })
     .then((code) => {
         console.log('PARSED CODE', code)
         this.props.handlers.handleHTMLUpdate(code.htmlString);
-        // console.log('WHAT IS THE HTML', code.htmlString)
         return code
     })
     .then((code) => {
@@ -89,16 +88,25 @@ export default class UserProjects extends Component {
           onRequestChange={(open) => this.setState({open})}
         >
           <h2>My Saved Projects</h2>
-          { this.state.projects.map((project) => {
-                  return (
+            {this.state.projects && this.state.projects.map((project) => {
+                return (
                     <MenuItem
                         key={project.id}
                         onClick={() => this.handleOpenProject(this.state.projects[project.id-1].hashedProjectId)}>
                     {project.hashedProjectId}
                     </MenuItem>)
-          })}
+            })}
         </Drawer>
       </div>
     );
   }
 }
+
+// {this.state.projects && this.state.projects.map((project) => {
+//                   return (
+//                     <MenuItem
+//                         key={project.id}
+//                         onClick={() => this.handleOpenProject(this.state.projects[project.id-1].hashedProjectId)}>
+//                     {project.hashedProjectId}
+//                     </MenuItem>)
+//           })}
