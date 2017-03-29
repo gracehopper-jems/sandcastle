fb81215a1ff34728b890be0ecbd3070acf535857
Jin Wang <jwang@Jins-MacBook-Air.local>
2017-03-29 13:50:14 -0400

    update modal

--- browser/Components/ShareButton.js
+++ browser/Components/ShareButton.js
@@ -6,26 +6,26 @@ import { Modal, Button } from 'react-bootstrap';
 
 export default class ShareButton extends Component {
 	constructor(props) {
-		super(props); 
+		super(props);
 		this.state = {
-			clicked: false, 
-			projectId: "", 
-			}; 
+			clicked: false,
+			projectId: "",
+			};
 		this.handleSave = this.handleSave.bind(this);
-		this.handleClose = this.handleClose.bind(this); 
+		this.handleClose = this.handleClose.bind(this);
 	}
 
 	handleSave(event) {
 		event.preventDefault();
 		const hashedProjectId = randomstring.generate(10);
 		const stringifiedCode = JSON.stringify(this.props.code);
-		this.setState({clicked:true}); 
-		this.setState({projectId: hashedProjectId}); 
+		this.setState({clicked:true});
+		this.setState({projectId: hashedProjectId});
 		axios.post(`/api/user/${this.props.user.userId}`, { code: stringifiedCode, hashedProjectId })
 	}
 
 	handleClose(event){
-		event.preventDefault(); 
+		event.preventDefault();
 		this.setState({clicked: false})
 	}
 
@@ -33,8 +33,8 @@ export default class ShareButton extends Component {
 		console.log("STATE", this.state);
 
 		return (
-			<div> 
-				{this.state.clicked ? 
+			<div>
+				{this.state.clicked ?
 
 				( <div className="static-modal">
 				    <Modal.Dialog>
@@ -51,11 +51,11 @@ export default class ShareButton extends Component {
 				      </Modal.Footer>
 
 				    </Modal.Dialog>
-				 </div> ) : 
+				 </div> ) :
 
 				( <div onClick={this.handleSave}>
 					Share
-				</div> ) 
+				</div> )
 				}
 			</div>
 		);
