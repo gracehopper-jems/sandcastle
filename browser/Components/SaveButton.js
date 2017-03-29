358f4f56c7f0158eb9f907a7f3a56b09dab510d3
mlkiung <meredith.kiung@gmail.com>
2017-03-29 16:17:54 -0400

    cleaned up code

--- browser/Components/SaveButton.js
+++ browser/Components/SaveButton.js
@@ -1,35 +1,24 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-const randomstring = require('randomstring');
-// import CustomProjectNameModal from '../Containers/CustomProjectNameModal';
-import store from '../store';
-import { Modal, Button } from 'react-bootstrap';
+// import React, { Component } from 'react';
+// import axios from 'axios';
+// const randomstring = require('randomstring');
+// import store from '../store';
+// import { Modal, Button } from 'react-bootstrap';
 
 
+// export default class SaveButton extends Component {
+// 	constructor(props) {
+// 		super(props);
 
-export default class SaveButton extends Component {
-	constructor(props) {
-		super(props);
+// 	}
+// 	render() {
+// 		return (
 
-	}
-	render() {
-		// const storeState = store.getState();
-		// const renderModal = storeState.loading.renderModal;
-		// console.log('RENDER MODAL?', renderModal);
-		return (
+// 				<div onClick={this.props.handleSaveModal}>
+// 					Save
+// 				</div>
 
-				<div onClick={this.props.handleSaveModal}>
-					Save
-				</div>
-
-		);
-	}
-}
+// 		);
+// 	}
+// }
 
 
-
-			// <div>
-			// 	<div>
-			// 		{renderModal ? <CustomProjectNameModal code={this.props.code} handlers={this.props.handlers} user={this.props.user} /> : null}
-			// 	</div>
-			// </div>
