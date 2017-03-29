0f283eda55f68918d22cc7c0fe5d33607c7c7402
mlkiung <meredith.kiung@gmail.com>
2017-03-29 17:13:37 -0400

    added a new reducer/action creator to be able to update state with all text editor strings at once

--- browser/Containers/AppContainer.js
+++ browser/Containers/AppContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { updateHTML, updateCSS, updateJS, updateServer, updateDatabase, updateCurrentProject } from '../reducers/code';
+import { updateHTML, updateCSS, updateJS, updateServer, updateDatabase, updateCurrentProject, updateAllCode } from '../reducers/code';
 import { sendJson, sendPost, sendClearDB, sendUpdateDockerOn, sendPort, sendDelete, sendUpdate } from '../reducers/docker.js';
 import { updateTimeForTourTrue, updateTimeForTourFalse } from '../reducers/loading';
 import { setUserId } from '../reducers/user';
@@ -79,6 +79,9 @@ const mapDispatchToProps = (dispatch) => {
         },
         handleCurrentProjectUpdate(...args) {
           dispatch(updateCurrentProject(...args));
+        },
+        handleUpdateAllCode(...args) {
+          dispatch(updateAllCode(...args));
         },
         handleSignin(...args) {
           dispatch(setUserId(...args));
