19d39b7f4b0c711f4a5c6db86f97da234472e2ca
Jin Wang <jwang@Jins-MacBook-Air.local>
2017-04-01 18:02:40 -0400

    adjust indentations in components

--- browser/Components/FirepadTabs.js
+++ browser/Components/FirepadTabs.js
@@ -5,34 +5,34 @@ import Editors from './Editors';
 
 export const FirepadTabs = () => {
 
-  const handleClick = (event) => {
-  }
+    const handleClick = (event) => {
+    }
 
-  let HTMLEditor = Editors[0];
-  let CSSEditor = Editors[1];
-  let JSEditor = Editors[2];
-  let ServerEditor = Editors[3];
-  let DatabaseEditor = Editors[4];
+    let HTMLEditor = Editors[0];
+    let CSSEditor = Editors[1];
+    let JSEditor = Editors[2];
+    let ServerEditor = Editors[3];
+    let DatabaseEditor = Editors[4];
 
-  return (
-    <div onClick={handleClick}>
-      <Tabs defaultActiveKey={1} id="FirepadTabContainer">
-        <Tab className="frontend-color" eventKey={1} title="HTML Editor">
-          <HTMLEditor />
-        </Tab>
-        <Tab className="frontend-color" eventKey={2} title="CSS Editor">
-          <CSSEditor />
-        </Tab>
-        <Tab className="frontend-color" eventKey={3} title="JS Editor">
-          <JSEditor />
-        </Tab>
-        <Tab className="backend-color" eventKey={4} title="Server Editor">
-          <ServerEditor />
-        </Tab>
-        <Tab className="backend-color" eventKey={5} title="Models Editor">
-          <DatabaseEditor />
-        </Tab>
-      </Tabs>
-    </div>
-  );
+    return (
+        <div onClick={handleClick}>
+            <Tabs defaultActiveKey={1} id="FirepadTabContainer">
+                <Tab className="frontend-color" eventKey={1} title="HTML Editor">
+                    <HTMLEditor />
+                </Tab>
+                <Tab className="frontend-color" eventKey={2} title="CSS Editor">
+                    <CSSEditor />
+                </Tab>
+                <Tab className="frontend-color" eventKey={3} title="JS Editor">
+                    <JSEditor />
+                </Tab>
+                <Tab className="backend-color" eventKey={4} title="Server Editor">
+                    <ServerEditor />
+                </Tab>
+                <Tab className="backend-color" eventKey={5} title="Models Editor">
+                    <DatabaseEditor />
+                </Tab>
+            </Tabs>
+        </div>
+    );
 };
