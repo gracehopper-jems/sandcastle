0d9bd3b3ea5595a3f32e5941c376001c2be76747
Jin Wang <jwang@Jins-MacBook-Air.local>
2017-04-01 18:53:58 -0400

    update routes

--- userRoutes.js
+++ userRoutes.js
@@ -5,49 +5,46 @@ const User = require('./models').User;
 const Project = require('./models').Project;
 
 router.post('/user/:firebaseId', (req, res, next) => {
-	User.findOrCreate({
-		where: {
-			firebaseId: req.params.firebaseId
-		}
-	})
-		.spread(user => {
-			Project.create(req.body)
-				.then(createdProject => {
-					createdProject.setUser(user);
-				})
-				.then(() => {
-					res.status(201);
-				})
-				.catch(next);
+		User.findOrCreate({
+				where: {
+						firebaseId: req.params.firebaseId
+				}
 		})
-		.catch(console.error);
+				.spread(user => {
+						Project.create(req.body)
+								.then(createdProject => {
+										createdProject.setUser(user);
+								})
+								.then(() => {
+										res.status(201);
+								})
+								.catch(next);
+				})
+				.catch(console.error);
 });
 
 router.get('/user/:firebaseId', (req, res, next) => {
-	User.findOne({ where: { firebaseId: req.params.firebaseId } })
-		.then(user => {
-			return user.getProjects();
-		})
-		.then(projects => {
-			res.status(200).send(projects);
-		})
-		.catch(next);
+		User.findOne({ where: { firebaseId: req.params.firebaseId } })
+				.then(user => {
+						return user.getProjects();
+				})
+				.then(projects => {
+						res.status(200).send(projects);
+				})
+				.catch(next);
 });
 
 router.get('/project/:hashedProjectId', (req, res, next) => {
-	Project.findOne({
-		where: {
-			hashedProjectId: req.params.hashedProjectId
-		}
-	})
-	.then( project => {
-		res.status(200).send(project);
-	})
-	.catch(console.error);
+		Project.findOne({
+				where: {
+						hashedProjectId: req.params.hashedProjectId
+				}
+		})
+		.then( project => {
+				res.status(200).send(project);
+		})
+		.catch(console.error);
 })
-
-
-
 
 module.exports = router;
 
