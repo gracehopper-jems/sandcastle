8c96be28761b1f49954c2ae8b52885a9a31fa4b6
Jin Wang <jwang@Jins-MacBook-Air.local>
2017-04-01 18:19:21 -0400

    adjust indentation for containers

--- browser/Containers/AppContainer.js
+++ browser/Containers/AppContainer.js
@@ -10,120 +10,120 @@ import NavbarContainer from './NavbarContainer';
 import WelcomeMessage from '../Components/WelcomeMessage';
 
 class AppContainer extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      renderWelcomeMessage: false
+    constructor(props){
+        super(props)
+        this.state = {
+            renderWelcomeMessage: false
+        }
     }
-  }
 
-  componentWillReceiveProps(nextProps) {
-    // if userid on props is empty and userid on nextprops is empty, then render welcome message
-    if (this.props.user.userId === '' && nextProps.user.userId === '') {
-      this.setState({renderWelcomeMessage: true});
+    componentWillReceiveProps(nextProps) {
+      // if userid on props is empty and userid on nextprops is empty, then render welcome message
+        if (this.props.user.userId === '' && nextProps.user.userId === '') {
+            this.setState({renderWelcomeMessage: true});
+        }
     }
-  }
 
-  render(){
-    return (
-        <div>
-            <NavbarContainer code={this.props.code} handlers={this.props.handlers} user={this.props.user} children={this.props.children} docker={this.props.docker}/>
-                {(this.props.user.userId === '' && this.state.renderWelcomeMessage === true)
-                ? <WelcomeMessage />
-                :
-                (<div id="start-tour-here" className='giant-container'>
-                    <div className='editor-container'>
-                        <FirepadTabs />
-                    </div>
-                    <div className='iframe-container'>
-                      <div className="container-fluid">
-                          <div className="row">
-                            <div className="col-md-6">
-                                <IframeTabs docker={this.props.docker} handlers={this.props.handlers} />
-                            </div>
+    render(){
+        return (
+            <div>
+                <NavbarContainer code={this.props.code} handlers={this.props.handlers} user={this.props.user} children={this.props.children} docker={this.props.docker}/>
+                  {(this.props.user.userId === '' && this.state.renderWelcomeMessage === true)
+                      ? <WelcomeMessage />
+                      :
+                      (<div id="start-tour-here" className='giant-container'>
+                          <div className='editor-container'>
+                              <FirepadTabs />
                           </div>
-                      </div>
-                    </div>
-                </div>
-                )}
-        </div>
-    );
-  }
+                          <div className='iframe-container'>
+                              <div className="container-fluid">
+                                  <div className="row">
+                                      <div className="col-md-6">
+                                          <IframeTabs docker={this.props.docker} handlers={this.props.handlers} />
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>)
+                  }
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    code: state.code,
-    user: state.user,
-    docker: state.docker
-  };
+    return {
+        code: state.code,
+        user: state.user,
+        docker: state.docker
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {handlers:
-      {
-        handleHTMLUpdate(...args) {
-          dispatch(updateHTML(...args));
-        },
-        handleCSSUpdate(...args) {
-          dispatch(updateCSS(...args));
-        },
-        handleJSUpdate(...args) {
-          dispatch(updateJS(...args));
-        },
-        handleServerUpdate(...args) {
-          dispatch(updateServer(...args));
-        },
-        handleDatabaseUpdate(...args) {
-          dispatch(updateDatabase(...args));
-        },
-        handleCurrentProjectUpdate(...args) {
-          dispatch(updateCurrentProject(...args));
-        },
-        handleUpdateAllCode(...args) {
-          dispatch(updateAllCode(...args));
-        },
-        handleSignin(...args) {
-          dispatch(setUserId(...args));
-        },
-        handleSignout(...args){
-          dispatch(setUserId(''));
-        },
-        handleSendJson(...args){
-          dispatch(sendJson(...args));
-        },
-        handleSendPost(...args){
-          dispatch(sendPost(...args));
-        },
-        handleSendClearDB(){
-          dispatch(sendClearDB());
-        },
-        handleUpdateDockerOn(...args){
-          dispatch(sendUpdateDockerOn(...args));
-        },
-        handleSendPort(...args){
-          dispatch(sendPort(...args));
-        },
-        handleSendDelete(...args){
-          dispatch(sendDelete(...args));
-        },
-        handleSendPut(...args){
-          dispatch(sendUpdate(...args));
-        },
-        handleSetTimeForTourTrue(...args) {
-          dispatch(updateTimeForTourTrue(...args));
-        },
-        handleSetTimeForTourFalse(...args) {
-          dispatch(updateTimeForTourFalse(...args));
-        },
-        handleUpdateRenderModalTrue(...args) {
-          dispatch(updateRenderModalTrue(...args));
-        },
-        handleUpdateRenderModalFalse(...args) {
-          dispatch(updateRenderModalFalse(...args));
+    return {handlers:
+        {
+            handleHTMLUpdate(...args) {
+                dispatch(updateHTML(...args));
+            },
+            handleCSSUpdate(...args) {
+                dispatch(updateCSS(...args));
+            },
+            handleJSUpdate(...args) {
+                dispatch(updateJS(...args));
+            },
+            handleServerUpdate(...args) {
+                dispatch(updateServer(...args));
+            },
+            handleDatabaseUpdate(...args) {
+                dispatch(updateDatabase(...args));
+            },
+            handleCurrentProjectUpdate(...args) {
+                dispatch(updateCurrentProject(...args));
+            },
+            handleUpdateAllCode(...args) {
+                dispatch(updateAllCode(...args));
+            },
+            handleSignin(...args) {
+                dispatch(setUserId(...args));
+            },
+            handleSignout(...args){
+                dispatch(setUserId(''));
+            },
+            handleSendJson(...args){
+                dispatch(sendJson(...args));
+            },
+            handleSendPost(...args){
+                dispatch(sendPost(...args));
+            },
+            handleSendClearDB(){
+                dispatch(sendClearDB());
+            },
+            handleUpdateDockerOn(...args){
+                dispatch(sendUpdateDockerOn(...args));
+            },
+            handleSendPort(...args){
+                dispatch(sendPort(...args));
+            },
+            handleSendDelete(...args){
+                dispatch(sendDelete(...args));
+            },
+            handleSendPut(...args){
+                dispatch(sendUpdate(...args));
+            },
+            handleSetTimeForTourTrue(...args) {
+                dispatch(updateTimeForTourTrue(...args));
+            },
+            handleSetTimeForTourFalse(...args) {
+                dispatch(updateTimeForTourFalse(...args));
+            },
+            handleUpdateRenderModalTrue(...args) {
+                dispatch(updateRenderModalTrue(...args));
+            },
+            handleUpdateRenderModalFalse(...args) {
+                dispatch(updateRenderModalFalse(...args));
+            }
         }
-      }
-  };
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(AppContainer);
--- browser/Containers/CustomProjectNameModal.js
+++ browser/Containers/CustomProjectNameModal.js
@@ -3,72 +3,66 @@ import axios from 'axios';
 const randomstring = require('randomstring');
 import { Modal, Button } from 'react-bootstrap';
 
-
-
 export default class CustomProjectNameModal extends Component {
-	constructor(props) {
-		super(props);
+		constructor(props) {
+				super(props);
 
-		this.state = {
-			projectName: '',
-		};
+				this.state = {
+						projectName: '',
+				};
 
-		this.handleSave = this.handleSave.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-	}
+				this.handleSave = this.handleSave.bind(this);
+				this.handleChange = this.handleChange.bind(this);
+		}
 
-	handleSave(event) {
-		event.preventDefault();
-		// post to db
-		const hashedProjectId = randomstring.generate(10);
-		// const projectName = this.state.projectName;
-		console.log('project name out of .then', this.state.projectName);
-		console.log('clicked save');
-		const stringifiedCode = JSON.stringify(this.props.code);
-		axios.post(`/api/user/${this.props.user.userId}`, {
-			code: stringifiedCode,
-			projectName: this.state.projectName,
-			hashedProjectId: hashedProjectId,
-		})
-			.then(() => {
-				console.log('posted successfully');
-				console.log('project name in .then', this.state.projectName);
-			})
-			.catch(console.error);
-		this.props.handleModalClose();
-	}
+		handleSave(event) {
+				event.preventDefault();
+				// post to db
+				const hashedProjectId = randomstring.generate(10);
+				// const projectName = this.state.projectName;
+				const stringifiedCode = JSON.stringify(this.props.code);
+				axios.post(`/api/user/${this.props.user.userId}`, {
+						code: stringifiedCode,
+						projectName: this.state.projectName,
+						hashedProjectId: hashedProjectId,
+				})
+				.then(() => {
+					console.log('posted successfully');
+				})
+				.catch(console.error);
+				this.props.handleModalClose();
+		}
 
-	handleChange(event) {
-		event.preventDefault();
-		this.setState({ projectName: event.target.value });
-	}
+		handleChange(event) {
+			event.preventDefault();
+			this.setState({ projectName: event.target.value });
+		}
 
-	render() {
-		return (
-			<div className="static-modal">
-				<Modal.Dialog>
-					<Modal.Header>
-						<Modal.Title>Please enter a name for your project:</Modal.Title>
-					</Modal.Header>
+		render() {
+				return (
+					<div className="static-modal">
+							<Modal.Dialog>
+									<Modal.Header>
+											<Modal.Title>Please enter a name for your project:</Modal.Title>
+									</Modal.Header>
 
-					<Modal.Body>
-						<form onSubmit={this.handleSave}>
-							<div className="form-group">
-								<label htmlFor="inlineFormInput">Name:</label>
-								<input name="project-name" type="text" className="form-control" id="inlineFormInput" placeholder="Project Name" onChange={this.handleChange} autoFocus={focus} value={this.state.projectName} />
-							</div>
+									<Modal.Body>
+											<form onSubmit={this.handleSave}>
+													<div className="form-group">
+															<label htmlFor="inlineFormInput">Name:</label>
+															<input name="project-name" type="text" className="form-control" id="inlineFormInput" placeholder="Project Name" onChange={this.handleChange} autoFocus={focus} value={this.state.projectName} />
+													</div>
 
-								<Button type="submit">Save</Button>
+													<Button type="submit">Save</Button>
 
-						</form>
-					</Modal.Body>
+											</form>
+									</Modal.Body>
 
-					<Modal.Footer>
-            <Button onClick={this.props.handleSaveModalClose}>Close</Button>
-         	</Modal.Footer>
-				</Modal.Dialog>
-			</div>
-		);
-	}
-
+									<Modal.Footer>
+				            	<Button onClick={this.props.handleSaveModalClose}>Close</Button>
+				         	</Modal.Footer>
+							</Modal.Dialog>
+					</div>
+				);
+		}
 }
