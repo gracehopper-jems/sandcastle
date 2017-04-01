13dd4cb9a8ee73fcfd1ab398499995c9ecb73766
Jin Wang <jin.wang.527@gmail.com>
2017-04-01 18:02:40 -0400

    adjust indentations in components

--- browser/Components/IframeTabs.js
+++ browser/Components/IframeTabs.js
@@ -4,33 +4,33 @@ import PostwomanContainer from '../Containers/PostwomanContainer';
 import DatabaseTable from '../Containers/DatabaseTable';
 
 export const IframeTabs = (props) => {
-  return (
-    <Tabs defaultActiveKey={1} id="IframeTabContainer">
-      <Tab eventKey={1} title="Frontend View">
-          <div id="frame" />
-      </Tab>
-      <Tab eventKey={2} title="Server View">
-        <div className="tab-div">
-          <PostwomanContainer className="test" handlers={props.handlers} docker={props.docker} />
-          <div>{props.docker ? props.docker.json : null}</div>
-        </div>
-      </Tab>
-      <Tab eventKey={3} title="Database View">
-        <div className="tab-div">
-          {!props.docker ? null
-            : props.docker.database.length === 0
-            ? null
-            : <DatabaseTable database={props.docker.database} />
-          }
-        </div>
-      </Tab>
-      <Tab eventKey={4} title="Full App View">
-        { !props.docker ? null
-          : props.docker.dockerOn === false
-          ? <div className="tab-div" />
-          : <iframe className="app-frame" src={`http://127.0.0.1:${props.docker.port}`} />
-          }
-      </Tab>
-    </Tabs>
-  )
+    return (
+        <Tabs defaultActiveKey={1} id="IframeTabContainer">
+            <Tab eventKey={1} title="Frontend View">
+                <div id="frame" />
+            </Tab>
+            <Tab eventKey={2} title="Server View">
+                <div className="tab-div">
+                    <PostwomanContainer className="test" handlers={props.handlers} docker={props.docker} />
+                    <div>{props.docker ? props.docker.json : null}</div>
+                </div>
+            </Tab>
+            <Tab eventKey={3} title="Database View">
+                <div className="tab-div">
+                    {!props.docker ? null
+                        : props.docker.database.length === 0
+                        ? null
+                        : <DatabaseTable database={props.docker.database} />
+                    }
+                </div>
+            </Tab>
+            <Tab eventKey={4} title="Full App View">
+                { !props.docker ? null
+                    : props.docker.dockerOn === false
+                    ? <div className="tab-div" />
+                    : <iframe className="app-frame" src={`http://127.0.0.1:${props.docker.port}`} />
+                }
+            </Tab>
+        </Tabs>
+    );
 };
