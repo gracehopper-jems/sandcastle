3c295f7c6b925e15833ec67d322021e09efc66bc
Susanna Huang <dearsusannahuang@gmail.com>
2017-04-04 17:58:51 -0400

    spacing is good

--- models/index.js
+++ models/index.js
@@ -2,28 +2,28 @@ const Sequelize = require('sequelize');
 const postgresPath = require('../variables').postgresPath;
 
 const db = new Sequelize(postgresPath, {
-		logging: false
+	logging: false
 });
 
 
 const User = db.define('user', {
-		firebaseId: {
-				type: Sequelize.STRING,
-				// allowNull: false,
-		}
+	firebaseId: {
+		type: Sequelize.STRING,
+		// allowNull: false,
+	}
 });
 
 const Project = db.define('project', {
-		hashedProjectId: {
-				type: Sequelize.STRING,
-				// allowNull: false,
-		},
-		code: {
-				type: Sequelize.JSON,
-		},
-		projectName: {
-				type: Sequelize.STRING
-		}
+	hashedProjectId: {
+		type: Sequelize.STRING,
+		// allowNull: false,
+	},
+	code: {
+		type: Sequelize.JSON,
+	},
+	projectName: {
+		type: Sequelize.STRING
+	}
 });
 
 Project.belongsTo(User);
