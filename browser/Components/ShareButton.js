3de6a50db742c52af3bd8e3f6c52badb56e908c4
Susanna Huang <dearsusannahuang@gmail.com>
2017-04-04 17:57:47 -0400

    fixed the indentations

--- browser/Components/ShareButton.js
+++ browser/Components/ShareButton.js
@@ -2,61 +2,58 @@ import React, { Component } from 'react';
 import axios from 'axios';
 const randomstring = require('randomstring');
 import { Modal, Button } from 'react-bootstrap';
-const appURL = require('../../variables').appURL; 
+const appURL = require('../../variables').appURL;
 
 
 export default class ShareButton extends Component {
-		constructor(props) {
-				super(props);
-				this.state = {
-						clicked: false,
-						projectId: "",
-				};
-				this.handleSave = this.handleSave.bind(this);
-				this.handleClose = this.handleClose.bind(this);
-		}
+	constructor(props) {
+		super(props);
+		this.state = {
+				clicked: false,
+				projectId: "",
+		};
+		this.handleSave = this.handleSave.bind(this);
+		this.handleClose = this.handleClose.bind(this);
+	}
 
-		handleSave(event) {
-				event.preventDefault();
-				const hashedProjectId = randomstring.generate(10);
-				const stringifiedCode = JSON.stringify(this.props.code);
-				this.setState({clicked:true});
-				this.setState({projectId: hashedProjectId});
-				axios.post(`/api/user/${this.props.user.userId}`, { code: stringifiedCode, hashedProjectId })
-		}
+	handleSave(event) {
+		event.preventDefault();
+		const hashedProjectId = randomstring.generate(10);
+		const stringifiedCode = JSON.stringify(this.props.code);
+		this.setState({clicked:true});
+		this.setState({projectId: hashedProjectId});
+		axios.post(`/api/user/${this.props.user.userId}`, { code: stringifiedCode, hashedProjectId })
+	}
 
-		handleClose(event){
-				event.preventDefault();
-				this.setState({clicked: false});
-		}
+	handleClose(event){
+		event.preventDefault();
+		this.setState({clicked: false});
+	}
 
-		render() {
-				return (
-						<div>
-								{this.state.clicked ?
+	render() {
+		return (
+			<div>
+				{this.state.clicked ?
+					( <div className="static-modal">
+						<Modal.Dialog>
+								<Modal.Header>
+									<Modal.Title>Share your project with this link:</Modal.Title>
+								</Modal.Header>
 
-									( <div className="static-modal">
-									    <Modal.Dialog>
-										      <Modal.Header>
-										        	<Modal.Title>Share your project with this link:</Modal.Title>
-										      </Modal.Header>
+								<Modal.Body>
+									{`${appURL}/share${this.state.projectId}`}
+								</Modal.Body>
 
-										      <Modal.Body>
-										        	{`${appURL}/share${this.state.projectId}`}
-										      </Modal.Body>
-
-										      <Modal.Footer>
-										        	<Button onClick={this.handleClose} >Close</Button>
-										      </Modal.Footer>
-
-									    </Modal.Dialog>
-									 </div> ) :
-
-									( <div onClick={this.handleSave}>
-											Share
-									</div> )
-								}
-						</div>
-				);
-		}
+								<Modal.Footer>
+									<Button onClick={this.handleClose} >Close</Button>
+								</Modal.Footer>
+						</Modal.Dialog>
+						</div> ) :
+					( <div onClick={this.handleSave}>
+							Share
+					</div> )
+				}
+			</div>
+		);
+	}
 }
--- browser/Containers/LoadingButton.js
+++ browser/Containers/LoadingButton.js
@@ -4,48 +4,48 @@ import tour from '../tour';
 import store from '../store';
 
 export default class LoadingButton extends Component {
-		constructor(props) {
-				super(props);
+	constructor(props) {
+		super(props);
 
-				this.state = {
-						isLoading: false
-				};
+		this.state = {
+			isLoading: false
+		};
 
-				this.handleClick = this.handleClick.bind(this);
-		}
+		this.handleClick = this.handleClick.bind(this);
+	}
 
-		handleClick() {
-				this.setState({isLoading: true});
-				updateIframeContents(this.props);
+	handleClick() {
+		this.setState({isLoading: true});
+		updateIframeContents(this.props);
 
-				setTimeout(() => {
-						// Completed of async action, set loading state back
-						this.setState({isLoading: false});
-				}, 2000);
-		}
+		setTimeout(() => {
+			// Completed of async action, set loading state back
+			this.setState({isLoading: false});
+		}, 2000);
+	}
 
-		render() {
-				let isLoading = this.state.isLoading;
-				return (
-						<div
-								disabled={isLoading}
-								onClick={!isLoading ? this.handleClick : null}>
-								{isLoading ? <ProgressModal view={'Frontend'} /> : 'Run Frontend'}
-						</div>
-				);
-		}
+	render() {
+		let isLoading = this.state.isLoading;
+		return (
+			<div
+				disabled={isLoading}
+				onClick={!isLoading ? this.handleClick : null}>
+				{isLoading ? <ProgressModal view={'Frontend'} /> : 'Run Frontend'}
+			</div>
+		);
+	}
 }
 
 function updateIframeContents(props) {
-		let currentIframe = document.getElementById('frame').children;
+	let currentIframe = document.getElementById('frame').children;
 
-		let getHTML = props.code.htmlString;
-		let CSS = '<style>' + props.code.cssString + '</style>';
-		let JS = '<script>' + props.code.jsString + '</script>';
+	let getHTML = props.code.htmlString;
+	let CSS = '<style>' + props.code.cssString + '</style>';
+	let JS = '<script>' + props.code.jsString + '</script>';
 
-		var myContent = '<!DOCTYPE html>' + '<html><head><title>Rendered HTML from Pattern</title>' + CSS + '</head><body>' + getHTML + JS + '</body></html>';
+	var myContent = '<!DOCTYPE html>' + '<html><head><title>Rendered HTML from Pattern</title>' + CSS + '</head><body>' + getHTML + JS + '</body></html>';
 
-		currentIframe[0].contentWindow.document.open('text/html', 'replace');
-		currentIframe[0].contentWindow.document.write(myContent);
-		currentIframe[0].contentWindow.document.close();
+	currentIframe[0].contentWindow.document.open('text/html', 'replace');
+	currentIframe[0].contentWindow.document.write(myContent);
+	currentIframe[0].contentWindow.document.close();
 }
