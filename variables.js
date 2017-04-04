e3c558e5041781c4f79bc9ee39fd367ce2856099
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-04-04 11:32:36 -0400

    started making branch deployable

--- variables.js
+++ variables.js
@@ -1,34 +1,30 @@
+/* 
+To run Sandcastle in a development environment, comment out the `deployed variables` and un-comment out the `production variables`. 
+
+WHEN YOU PUSH TO MASTER, BE SURE TO COMMENT THE DEPLOYMENT VARIABLES BACK IN, AND THE PRODUCTION VARIABLES OUT!!!
+	
+
+	====================
+	DEPLOYMENT VARIABLES
+	====================  */
+
+
+const postgresPath = 'postgres://sandcastles:sandcastles@localhost:5432/sandcastles'
+
+const appURL = 'http://www.sandcastle.world'; 
+
+
 /*  ====================
 	PRODUCTION VARIABLES
 	====================  */
 
 
-const postgresPath = 'postgres://localhost:5432/sandcastle';
+// const postgresPath = 'postgres://localhost:5432/sandcastle' 
 
-const dockerCmd = 'docker';
-
-const ipAddress = 'http://localhost:8080';
-
-const appURL = 'http://localhost:3000';
-
-
-
-/*  ====================
-	DEPLOYED VARIABLES
-	====================  */
-
-// const postgresPath = 'postgres://sandcastles:sandcastles@localhost:5432/sandcastles'
-
-// const dockerCmd = 'sudo docker';
-
-// const ipAddress = ??????
-
-// const appURL = 'http://www.sandcastle.world';
-
+// const appURL = 'http://localhost:3000';
 
 
 module.exports = {
   	postgresPath,
-  	dockerCmd,
-  	ipAddress
+  	appURL
 };
